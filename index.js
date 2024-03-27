c305055ef457926a802c98e9ea2eea95fc1992e6
nevilrv <nevilrv@gmail.com>
2024-03-27 18:32:43 +0530

    add muteand hidecamera

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const socket = require("socket.io")
+const socket = require("socket.io");
 
 const server = app.listen(3000, () => {
-    console.log('server is running..');
-})
+  console.log("server is running..");
+});
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const userRoute = require('./routes/userRoute');
-app.use('/', userRoute);
+const userRoute = require("./routes/userRoute");
+app.use("/", userRoute);
 
-var io = socket(server)
+var io = socket(server);
 io.on("connection", function (socket) {
-    console.log('User Connected: ' + socket.id);
+  console.log("User Connected: " + socket.id);
 
-    socket.on('Join', function (roomName) {
-        var rooms = io.sockets.adapter.rooms
-        var room = rooms.get(roomName);
-        if (room === undefined) {
-            socket.join(roomName)
-            socket.emit("created")
-        } else if (room.size == 1) {
-            socket.join(roomName)
-            socket.emit("joined")
-        } else {
-            socket.emit("full")
-        }
-    })
+  socket.on("Join", function (roomName) {
+    var rooms = io.sockets.adapter.rooms;
+    var room = rooms.get(roomName);
+    if (room === undefined) {
+      socket.join(roomName);
+      socket.emit("created");
+    } else if (room.size == 1) {
+      socket.join(roomName);
+      socket.emit("joined");
+    } else {
+      socket.emit("full");
+    }
+  });
 
-    socket.on('ready', function (roomName) {
-        console.log("ready");
-        socket.broadcast.to(roomName).emit('ready')
-    })
+  socket.on("ready", function (roomName) {
+    console.log("ready");
+    socket.broadcast.to(roomName).emit("ready");
+  });
 
-    socket.on('candidate', function (candidate, roomName) {
-        console.log("candidate");
-        console.log(candidate);
-        socket.broadcast.to(roomName).emit("candidate", candidate)
-    })
+  socket.on("candidate", function (candidate, roomName) {
+    console.log("candidate");
+    console.log(candidate);
+    socket.broadcast.to(roomName).emit("candidate", candidate);
+  });
 
-    socket.on("offer", function (offer, roomName) {
-        console.log("offer");
-        socket.broadcast.to(roomName).emit("offer", offer)        
-    })
+  socket.on("offer", function (offer, roomName) {
+    console.log("offer");
+    socket.broadcast.to(roomName).emit("offer", offer);
+  });
 
-    socket.on("answer", function (answer, roomName) {
-        socket.broadcast.to(roomName).emit("answer", answer)
-    })
-})
+  socket.on("answer", function (answer, roomName) {
+    socket.broadcast.to(roomName).emit("answer", answer);
+  });
+
+  // leave room code
+  socket.on("leave", function (roomName) {
+    socket.leave(roomName);
+    socket.broadcast.to(roomName).emit("leave");
+  });
+});
--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,14 @@ var joinBtn = document.getElementById("join");
 var roomInput = document.getElementById("roomName");
 var userVideo = document.getElementById("user-video");
 var peerVideo = document.getElementById("peer-video");
+// upgrade work
+var Button_Group = document.getElementById("Button_Group");
+var MuteBtn = document.getElementById("muteBtn");
+var Hidecamara = document.getElementById("Hidecamara");
+var LeaveRoombtn = document.getElementById("LeaveRoom");
+
+var mutefleg = false;
+var hidecamareflg = false;
 
 var roomName = roomInput.value;
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
@@ -29,19 +37,47 @@ joinBtn.addEventListener("click", function () {
   }
 });
 
+MuteBtn.addEventListener("click", function () {
+  mutefleg = !mutefleg;
+  if (mutefleg) {
+    MuteBtn.textContent = "Un Mute Mic";
+    userStream.getTracks()[0].enabled = false;
+  } else {
+    MuteBtn.textContent = "Mute Mic";
+    userStream.getTracks()[0].enabled = true;
+  }
+});
+
+Hidecamara.addEventListener("click", function () {
+  hidecamareflg = !hidecamareflg;
+  if (hidecamareflg) {
+    Hidecamara.textContent = "Showcamara";
+    userStream.getTracks()[1].enabled = false;
+    Hidecamara.poster =
+      "https://southsafe.ie/wp-content/uploads/2021/02/Lenovo.png";
+    // const image = document.createElement("img");
+    // image.src = "https://southsafe.ie/wp-content/uploads/2021/02/Lenovo.png";
+    // document.querySelector("#user-video").appendChild(image);
+  } else {
+    Hidecamara.textContent = "Hide camara";
+    userStream.getTracks()[1].enabled = true;
+  }
+});
+
 socket.on("created", function () {
   creator = true;
   navigator.getUserMedia(
     {
       audio: true,
       video: {
-        width: 1280,
-        height: 720,
+        width: 500,
+        height: 500,
       },
     },
     function (stream) {
       userStream = stream;
       videoChatForm.style = "display:none";
+      Button_Group.style = "display:flex";
       userVideo.srcObject = stream;
       userVideo.onloadedmetadata = function (e) {
         userVideo.play();
@@ -60,13 +96,14 @@ socket.on("joined", function () {
     {
       audio: true,
       video: {
-        width: 1280,
-        height: 720,
+        width: 500,
+        height: 500,
       },
     },
     function (stream) {
       userStream = stream;
       videoChatForm.style = "display:none";
+      Button_Group.style = "display:flex";
       userVideo.srcObject = stream;
       userVideo.onloadedmetadata = function (e) {
         userVideo.play();
@@ -90,6 +127,7 @@ socket.on("ready", function () {
     rtcPeerConnection.ontrack = onTrackFunction;
     rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream); // for audio
     rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream); // for video
+    console.log(userStream.getTracks()[1], "======");
     rtcPeerConnection.createOffer(
       function (offer) {
         rtcPeerConnection.setLocalDescription(offer);
@@ -114,6 +152,7 @@ socket.on("offer", function (offer) {
     rtcPeerConnection.ontrack = onTrackFunction;
     rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream); // for audio
     rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream); // for video
+    console.log(userStream.getTracks()[1], "======");
     rtcPeerConnection.setRemoteDescription(offer);
     rtcPeerConnection.createAnswer(
       function (answer) {
@@ -130,6 +169,23 @@ socket.on("offer", function (offer) {
 socket.on("answer", function (answer) {
   rtcPeerConnection.setRemoteDescription(answer);
 });
+
+// Leave btn
+LeaveRoombtn.addEventListener("click", function () {
+  socket.emit("leave", roomName);
+  videoChatForm.style = "display:block";
+  Button_Group.style = "display:none";
+
+  if (userVideo.srcObject) {
+    userVideo.srcObject.getTracks()[0].stop();
+    userVideo.srcObject.getTracks()[1].stop();
+  }
+  if (peerVideo.srcObject) {
+    peerVideo.srcObject.getTracks()[0].stop();
+    peerVideo.srcObject.getTracks()[1].stop();
+  }
+});
+socket.on("leave", function () {});
 
 function OnIceCandidateFunction(event) {
   console.log("🚀 ~ OnIceCandidateFunction ~ event:", event.candidate);
